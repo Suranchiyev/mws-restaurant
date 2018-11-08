6b3a34bba2c6076fe363a1da4d83f8819db12165
Beknazar Suranchiyev <Suranchiyev96@gmail.com>
2018-11-08 07:42:10 -0500

    Set up token for maps

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiZ2F1YndzIiwiYSI6ImNqbzhrazg2YzE0cDczcHB1YzdsdzF6d2MifQ.dm5hO5Qc-uf6nvs7_XUacA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
